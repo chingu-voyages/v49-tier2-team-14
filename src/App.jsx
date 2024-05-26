e5b86a8af0033bf13087e77216ae727c872a802a
Efezino Idisi <enduranceidisi13@gmail.com>
2024-05-27 00:42:23 +0100

    integrate color picker api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import MainContent from "./components/MainContent/MainContent";
 
 function App() {
   return (
     <>
       <Header />
-      <h1>Hello I'm a test app</h1>
+      <MainContent />
       <Footer />
     </>
   );
--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-function Footer() {
-    return (
-        <p>Copyright 2024 Lakshmidevi, Miko, Nemanja, Zee</p>
-    )
-}
-
-export default Footer
