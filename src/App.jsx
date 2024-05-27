9bd2bebb3befb9fe2ec62173ca4edc7194ca7d43
devi-lakshmi <devi.venki8@gmail.com>
2024-05-27 17:53:27 +0200

    created colorhormony dropdown menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-
+import ColorHarmony from "./components/ColorHarmony/ColorHarmony";
 function App() {
   return (
     <>
       <Header />
-      <h1>Hello I'm a test app</h1>
+      <h1>Hello I am a test app</h1>
+      <ColorHarmony/>
       <Footer />
     </>
   );
